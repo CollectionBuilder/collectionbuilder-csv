e98693a8e84eebf87b87c89abb509527e98685d4
evanwill <evanpeterw@gmail.com>
2022-05-01 16:21:03 -0700

    add title to icons

--- _includes/js/get-icon.js
+++ _includes/js/get-icon.js
@@ -3,7 +3,7 @@
     return svg sprite
 */
 function getIcon(objectTemplate,objectFormat) {
-    var iconTemplate, iconId;
+    var iconTemplate, iconId, iconTitle;
     if (objectTemplate && objectTemplate != "") {
         iconTemplate = objectTemplate;
     } else if (objectFormat && objectFormat != "") {
@@ -14,15 +14,20 @@ function getIcon(objectTemplate,objectFormat) {
     // choose icon
     if (iconTemplate.includes("image")) {
         iconId = "icon-image";
+        iconTitle = "image file icon";
     } else if (iconTemplate.includes("pdf")) {
         iconId = "icon-pdf";
+        iconTitle = "pdf file icon";
     } else if (iconTemplate.includes("video")) {
         iconId = "icon-video";
+        iconTitle = "video file icon";
     } else if (iconTemplate.includes("audio")) {
         iconId = "icon-audio";
+        iconTitle = "audio file icon";
     } else {
         iconId = "icon-default";
+        iconTitle = "file icon";
     }
     // svg sprite as thumb
-    return '<svg class="bi text-body" fill="currentColor"><use xlink:href="{{ "/assets/lib/cb-icons.svg" | relative_url }}#' + iconId + '"/></svg>';
+    return '<svg class="bi text-body" fill="currentColor" role="img"><title>' + iconTitle + '</title><use xlink:href="{{ "/assets/lib/cb-icons.svg" | relative_url }}#' + iconId + '"/></svg>';
 }
