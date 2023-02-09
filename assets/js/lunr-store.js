7f360c06a5c2b614371ce2702431525703ebdadb
Devin Becker <devinbecker@gmail.com>
2023-02-08 16:26:06 -0800

    fix lunr store to use theme and create correct links

--- assets/js/lunr-store.js
+++ assets/js/lunr-store.js
@@ -1,13 +1,17 @@
 ---
 # create lunr store for search page
 ---
+{% if site.data.theme.search-child-objects == false %}
+{%- assign items = site.data[site.metadata] | where_exp: 'item','item.parentid == nil' -%}
+{% else %}
 {%- assign items = site.data[site.metadata] | where_exp: 'item','item.objectid' -%}
+{% endif %}
 {%- assign fields = site.data.config-search -%}
 var store = [ 
 {%- for item in items -%} 
 {  
 {% for f in fields %}{% if item[f.field] %}{{ f.field | jsonify }}: {{ item[f.field] | normalize_whitespace | replace: '""','"' | jsonify }},{% endif %}{% endfor %} 
-"id": {{ item.objectid | jsonify }}
+"id": {% if item.parentid %}{{ item.parentid | append: '.html#' | append: item.objectid | jsonify }}{% else %}{{item.objectid | append: '.html' | jsonify }}{% endif %}
 
 }{%- unless forloop.last -%},{%- endunless -%}
 {%- endfor -%}
