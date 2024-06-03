6852f264c95d89a52baffa3f68988ddd2a136a39
Moritz Mähr <maehr@users.noreply.github.com>
2024-06-03 10:02:05 +0200

    Export omeka (#101)
    * feat: update export script to new data model
    
    * fix: update github actions #100
    
    * fix: typo and guard
    
    * feat: geodata layout added
    
    * feat: label uri format changed
    
    * fix: dc identifier instead omeka id
    
    * fix: remove quotation marks from csv
    
    * fix: guard for invalid url added
    
    * fix: template selection based on mimetype
    
    * feat: avoid redownload of images
    
    * fix: fixme added
    
    * fix: fix #106
    
    * fix: object location removed for parent
    
    * feat: logging added
    
    * fix: escape ;

--- .github/workflows/process_data.py
+++ .github/workflows/process_data.py
@@ -1,155 +1,255 @@
+import logging
 import os
+from urllib.parse import urljoin, urlparse
+
 import pandas as pd
 import requests
-from urllib.parse import urlparse, parse_qs
 
-OMEKA_API_URL = os.environ.get("OMEKA_API_URL")
-KEY_IDENTITY = os.environ.get("KEY_IDENTITY")
-KEY_CREDENTIAL = os.environ.get("KEY_CREDENTIAL")
-ITEM_SET_ID = os.environ.get("ITEM_SET_ID")   
+# Configuration
+OMEKA_API_URL = os.getenv("OMEKA_API_URL")
+KEY_IDENTITY = os.getenv("KEY_IDENTITY")
+KEY_CREDENTIAL = os.getenv("KEY_CREDENTIAL")
+ITEM_SET_ID = os.getenv("ITEM_SET_ID")
 
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+# Function to get items from a collection
 def get_items_from_collection(collection_id):
-    url = OMEKA_API_URL + "items"
+    url = urljoin(OMEKA_API_URL, "items")
     all_items = []
     params = {
         "item_set_id": collection_id,
         "key_identity": KEY_IDENTITY,
         "key_credential": KEY_CREDENTIAL,
+        "per_page": 100,
     }
 
     while True:
         response = requests.get(url, params=params)
         if response.status_code != 200:
-            print(f"Error: {response.status_code}")
+            logging.error(f"Error: {response.status_code}")
             break
-
-        # Add the items from the current page to our list
-        all_items.extend(response.json())
-
-        # Check if there is a 'next' page
-        links = requests.utils.parse_header_links(response.headers.get("Link", ""))
-        next_link = [link for link in links if link["rel"] == "next"]
-        if not next_link:
+        items = response.json()
+        all_items.extend(items)
+        next_url = None
+        for link in response.links:
+            if response.links[link]["rel"] == "next":
+                next_url = response.links[link]["url"]
+                break
+        if not next_url:
             break
-
-        # Update the URL for the next request
-        next_url = next_link[0]["url"]
-        url_parsed = urlparse(next_url)
-        next_params = parse_qs(url_parsed.query)
-        params.update(next_params)
-
+        url = next_url
     return all_items
 
 
+# Function to get media for an item
 def get_media(item_id):
-    url = OMEKA_API_URL + "media/" + str(item_id)
+    url = urljoin(OMEKA_API_URL, f"media?item_id={item_id}")
     params = {"key_identity": KEY_IDENTITY, "key_credential": KEY_CREDENTIAL}
     response = requests.get(url, params=params)
     if response.status_code != 200:
-        print(f"Error: {response.status_code}")
+        logging.error(f"Error: {response.status_code}")
         return None
     return response.json()
 
 
-def media_type(media_list, index=None):
-    if not media_list:
-        return "record"
-    if len(media_list) > 1 and index is None:
-        media_types = [media.get("o:media_type") for media in media_list]
-        if all([media_type.startswith("image") for media_type in media_types]):
-            return "multiple"
-        return "compound_object"
-    media = media_list[index] if index is not None else media_list[0]
-    media_type = media.get("o:media_type")
-    if media_type.startswith("image"):
-        return "image"
-    if media_type.startswith("audio"):
-        return "audio"
-    if media_type.startswith("video"):
-        return "video"
-    if media_type.startswith("application/pdf"):
-        return "pdf"
-    return "record"
+# Function to download file
+def download_file(url, dest_path):
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    try:
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            with open(dest_path, "wb") as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+    except requests.exceptions.HTTPError as http_err:
+        logging.error(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        logging.error(f"Other error occurred: {err}")
 
 
-def map_columns(item_set):
-    return_list = []
-    for data in item_set:
-        media_list = [
-            get_media(media_item.get("o:id")) for media_item in data.get("o:media", [])
-        ]
-        type = media_type(media_list)
-        media = media_list[0] if media_list else {}
-        media_data = create_media_data_dict(data, media, type)
-        return_list.append(media_data)
-        if len(media_list) > 1:
-            for index, media in enumerate(media_list):
-                type_child = media_type(media_list, index)
-                media_data = create_media_data_dict(data, media, type_child, index)
-                return_list.append(media_data)
-    return return_list
+# Function to check if URL is valid
+def is_valid_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
 
 
-def create_media_data_dict(data, media, type, index=None):
-    media_id_suffix = f"_{index}" if index is not None else ""
-    media_url = (
-        media.get("thumbnail_display_urls", {}).get("large")
-        if media
-        and media.get("thumbnail_display_urls", {}).get("large", "").startswith("http")
-        else None
+# Helper functions to extract data
+def extract_prop_value(props, prop_id):
+    return next(
+        (
+            prop.get("@value", "")
+            for prop in props
+            if prop.get("property_id") == prop_id
+        ),
+        "",
     )
-    media_original_url = (
-        media.get("o:original_url")
-        if media and
-        (len(data.get("o:media", [])) > 1 and
-         index is not None) and
-        media.get("o:original_url", "").startswith("http") and media.get("o:is_public")
-        else None
+
+
+def extract_prop_uri(props, prop_id):
+    return next(
+        (
+            f"[{prop.get('o:label', '')}]({prop.get('@id', '')})"
+            for prop in props
+            if prop.get("property_id") == prop_id
+        ),
+        "",
     )
-    media_title = (
-        media.get("o:title")
-        if index is not None
-        else data.get("dcterms:title", [{}])[0].get("@value")
-    )
+
+
+def extract_combined_list(props):
+    values = [
+        prop.get("@value", "").replace(";", "&#59")
+        for prop in props
+        if "@value" in prop
+    ]
+    uris = [
+        f"[{prop.get('o:label', '').replace(';', '&#59')}]({prop.get('@id', '').replace(';', '&#59')})"
+        for prop in props
+        if "@id" in prop
+    ]
+    combined = values + uris
+    return ";".join(combined)
+
+
+def extract_item_data(item):
+    # Download the thumbnail image if available and valid
+    image_url = item.get("thumbnail_display_urls", {}).get("large", "")
+    local_image_path = ""
+    if image_url and is_valid_url(image_url):
+        filename = os.path.basename(image_url)
+        local_image_path = f"objects/{filename}"
+        if not os.path.exists(local_image_path):
+            download_file(image_url, local_image_path)
+
+    logging.info(f"Item ID: {item['o:id']}")
 
     return {
-        "objectid": data.get("dcterms:identifier", [{}])[0].get("@value").lower()
-        + media_id_suffix,
-        "parentid": None
-        if index is None
-        else data.get("dcterms:identifier", [{}])[0].get("@value").lower(),
-        "title": media_title,
-        "creator": data.get("dcterms:creator", [{}])[0].get("@value"),
-        "date": data.get("dcterms:date", [{}])[0].get("@value"),
-        "era": data.get("dcterms:temporal", [{}])[0].get("@value"),
-        "description": data.get("dcterms:description", [{}])[0].get("@value"),
-        "subject": "; ".join(
-            [item.get("@value", "") for item in data.get("dcterms:subject", [])]
-        ),
-        "publisher": data.get("dcterms:publisher", [{}])[0].get("@value"),
-        "source": data.get("dcterms:source", [{}])[0].get("@value"),
-        "relation": data.get("dcterms:relation", [{}])[0].get("@value"),
-        "hasVersion": data.get("dcterms:hasVersion", [{}])[0].get("@value"),
-        "type": data.get("dcterms:type", [{}])[0].get("@id"),
-        "format": data.get("dcterms:format", [{}])[0].get("@value"),
-        "extent": data.get("dcterms:extent", [{}])[0].get("@value"),
-        "language": data.get("dcterms:language", [{}])[0].get("o:label"),
-        "rights": "; ".join(
-            [item.get("@value", "") for item in data.get("dcterms:rights", [])]
-        ),
-        "license": data.get("dcterms:license", [{}])[0].get("@value"),
-        "isPartOf": data.get("dcterms:isPartOf", [{}])[0].get("@value"),
-        "isReferencedBy": data.get("dcterms:isReferencedBy", [{}])[0].get("@value"),
-        "display_template": type,
-        "object_location": media_original_url,
-        "image_small": media_url,
-        "image_thumb": media_url,
-        "image_alt_text": None,
-        "object_transcript": None,
+        "objectid": extract_prop_value(item.get("dcterms:identifier", []), 10),
+        "parentid": "",
+        "title": extract_prop_value(item.get("dcterms:title", []), 1),
+        "description": extract_prop_value(item.get("dcterms:description", []), 4),
+        "subject": extract_combined_list(item.get("dcterms:subject", [])),
+        "era": extract_prop_value(item.get("dcterms:temporal", []), 41),
+        "isPartOf": extract_combined_list(item.get("dcterms:isPartOf", [])),
+        "creator": extract_combined_list(item.get("dcterms:creator", [])),
+        "publisher": extract_combined_list(item.get("dcterms:publisher", [])),
+        "source": extract_combined_list(item.get("dcterms:source", [])),
+        "date": extract_prop_value(item.get("dcterms:date", []), 7),
+        "type": extract_prop_uri(item.get("dcterms:type", []), 8),
+        "format": extract_prop_value(item.get("dcterms:format", []), 9),
+        "extent": extract_prop_value(item.get("dcterms:extent", []), 25),
+        "language": extract_prop_value(item.get("dcterms:language", []), 12),
+        "relation": extract_combined_list(item.get("dcterms:relation", [])),
+        "rights": extract_prop_value(item.get("dcterms:rights", []), 15),
+        "license": extract_prop_value(item.get("dcterms:license", []), 49),
+        "display_template": "compound_object",
+        "object_location": "",
+        "image_small": local_image_path,
+        "image_thumb": local_image_path,
+        "image_alt_text": item.get("o:alt_text", ""),
     }
 
 
-item_set = get_items_from_collection(ITEM_SET_ID)
-item_set_mapped = map_columns(item_set)
-item_set_mapped_df = pd.DataFrame(item_set_mapped)
-item_set_mapped_df.to_csv("_data/sgb-metadata.csv", index=False)
+def infer_display_template(format_value):
+    if "image" in format_value.lower():
+        return "image"
+    elif "pdf" in format_value.lower():
+        return "pdf"
+    elif "geo+json" in format_value.lower():
+        return "geodata"
+    else:
+        return "record"
+
+
+def extract_media_data(media, item_dc_identifier):
+    format_value = extract_prop_value(media.get("dcterms:format", []), 9)
+    display_template = infer_display_template(format_value)
+
+    # Download the thumbnail image if available and valid
+    image_url = media.get("thumbnail_display_urls", {}).get("large", "")
+    local_image_path = ""
+    if image_url and is_valid_url(image_url):
+        filename = os.path.basename(image_url)
+        local_image_path = f"objects/{filename}"
+        if not os.path.exists(local_image_path):
+            download_file(image_url, local_image_path)
+
+    # Extract media data
+    object_location = (
+        media.get("o:original_url", "") if media.get("o:is_public", False) else ""
+    )
+
+    logging.info(f"Media ID: {media['o:id']}")
+    logging.info(f"is_public: {media.get('o:is_public')}")
+
+    return {
+        "objectid": extract_prop_value(media.get("dcterms:identifier", []), 10),
+        "parentid": item_dc_identifier,
+        "title": extract_prop_value(media.get("dcterms:title", []), 1),
+        "description": extract_prop_value(media.get("dcterms:description", []), 4),
+        "subject": extract_combined_list(media.get("dcterms:subject", [])),
+        "era": extract_prop_value(media.get("dcterms:temporal", []), 41),
+        "isPartOf": extract_combined_list(media.get("dcterms:isPartOf", [])),
+        "creator": extract_combined_list(media.get("dcterms:creator", [])),
+        "publisher": extract_combined_list(media.get("dcterms:publisher", [])),
+        "source": extract_combined_list(media.get("dcterms:source", [])),
+        "date": extract_prop_value(media.get("dcterms:date", []), 7),
+        "type": extract_prop_uri(media.get("dcterms:type", []), 8),
+        "format": format_value,
+        "extent": extract_prop_value(media.get("dcterms:extent", []), 25),
+        "language": extract_prop_value(media.get("dcterms:language", []), 12),
+        "relation": extract_combined_list(media.get("dcterms:relation", [])),
+        "rights": extract_prop_value(media.get("dcterms:rights", []), 15),
+        "license": extract_prop_value(media.get("dcterms:license", []), 49),
+        "display_template": display_template,
+        "object_location": object_location,
+        "image_small": local_image_path,
+        "image_thumb": local_image_path,
+        "image_alt_text": media.get("o:alt_text", ""),
+    }
+
+
+# Main function to download item set and generate CSV
+def main():
+    # Download item set
+    collection_id = ITEM_SET_ID
+    items_data = get_items_from_collection(collection_id)
+
+    # Extract item data
+    item_records = []
+    media_records = []
+    for item in items_data:
+        item_record = extract_item_data(item)
+        item_records.append(item_record)
+
+        # Extract media data for each item
+        item_dc_identifier = item_record["objectid"]
+        media_data = get_media(item["o:id"])
+        if media_data:
+            for media in media_data:
+                media_records.append(extract_media_data(media, item_dc_identifier))
+
+    # Combine item and media records
+    combined_records = item_records + media_records
+
+    # Create DataFrame
+    df = pd.DataFrame(combined_records)
+
+    # Save to CSV
+    csv_path = "_data/sgb-metadata.csv"
+    os.makedirs(os.path.dirname(csv_path), exist_ok=True)
+    df.to_csv(csv_path, index=False)
+
+    logging.info(f"CSV file has been saved to {csv_path}")
+
+
+if __name__ == "__main__":
+    main()
